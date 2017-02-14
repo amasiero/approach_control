2f18ba1d64234bc358f4e1bd7db4ae0fad68a109
Andrey Masiero <andreymasiero@gmail.com>
2017-02-14 09:46:57 -0800

    approach_contol_walk package created

---
+++ approach_control_walk/nodes/approach_control_walk/Walk.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+import time
+from p2os_msgs.msg import MotorState
+from geometry_msgs.msg import Twist
+
+class Walk(smach.State):
+	def __init__(self, outcomes=['success','fail'], input_keys=['robot_speed']):
+		smach.State.__init__(self, outcomes=['success','fail'], input_keys=['robot_speed'])
+		self.motor_state_pub = rospy.Publisher('/cmd_motor_state', MotorState, queue_size=10)
+		self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+
+	def execute(self, userdata):
+		rospy.loginfo('Walking Robot')
+		time.sleep(0.1)
+
+		velocity = Twist()
+		motor_state = MotorState()
+		motor_state.state = 4
+		self.motor_state_pub.publish(motor_state)
+
+		velocity.linear.x = 0.0
+		velocity.angular.z = 0.0
+
+		if userdata.robot_speed == 'fast':
+			velocity.linear.x = 0.5
+			self.cmd_vel_pub.publish(velocity)
+			return 'success'
+		elif userdata.robot_speed == 'slow':
+			velocity.linear.x = 0.2
+			self.cmd_vel_pub.publish(velocity)
+			return 'success'
+		else:
+			return 'fail'
+
