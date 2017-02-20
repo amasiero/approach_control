2f302b334fc45c7a7f0eaa68c65e8027c1ebcc1c
Andrey Masiero <andreymasiero@gmail.com>
2017-02-20 11:41:14 -0800

    small fixes

--- approach_control_people/setup.py
+++ approach_control_people/setup.py
@@ -1,13 +1,12 @@
 ## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
 
-from disutils.core import setup
-from catkin_pkg.python_setup import generate_disutils_setup
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
-
-setup_args = generate_disutils_setup(
-	packages=['approach_control_people'],
-	package_dir={'':'nodes'},
+setup_args = generate_distutils_setup(
+    packages=['approach_control_people'],
+    package_dir={'': 'nodes'},
 )
 
 setup(**setup_args)
