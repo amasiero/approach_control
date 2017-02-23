2c42ae8c071d0009504d571ee67642e6cacdc744
Andrey Masiero <andreymasiero@gmail.com>
2017-02-23 15:11:28 -0300

    Refactoring movement package

---
+++ approach_control_movement/nodes/approach_control_movement/walk/Walk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import rospy
+import smach
+import smach_ros
+import time
+from p2os_msgs.msg import MotorState
+from geometry_msgs.msg import Twist
+
+class Walk(smach.State):
+	def __init__(self, linear=0.0, angular=0.0):
+		smach.State.__init__(self, outcomes=['walking'])
+		self.motor_state_pub = rospy.Publisher('/cmd_motor_state', MotorState, queue_size=10)
+		self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+
+		self.linear = linear
+		self.angular = angular
+
+
+	def execute(self, userdata=None):
+		rospy.loginfo('Robot is walking...')
+		time.sleep(0.1)
+
+		velocity = Twist()
+		motor_state = MotorState()
+		motor_state.state = 4
+		self.motor_state_pub.publish(motor_state)
+
+		velocity.linear.x = self.linear
+		velocity.angular.z = self.angular
+		self.cmd_vel_pub.publish(velocity)
+		
+		return 'walking'
+		
--- approach_control_movement/setup.py
+++ approach_control_movement/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['approach_control_walk'],
+    packages=['approach_control_movement'],
     package_dir={'': 'nodes'},
 )
 
