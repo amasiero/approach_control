332ee36d3bb65dff51ad2917958a3416d474b90a
Andrey Masiero <andreymasiero@gmail.com>
2017-02-24 10:57:22 -0300

    Laser class small fixes

--- approach_control_sensors/nodes/approach_control_sensors/Laser.py
+++ approach_control_sensors/nodes/approach_control_sensors/Laser.py
@@ -6,10 +6,10 @@ import smach_ros
 import time
 from sensor_msgs.msg import LaserScan
 
-class LaserFront(smach.State):
+class Laser(smach.State):
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['closerFront', 'closerRight', 'closerLeft', 'far'])
-		rospy.Subscriber('/scan', LaserScan, readLaser)
+		rospy.Subscriber('/scan', LaserScan, self.readLaser)
 		self.ranges = 0.0
 
 	def readLaser(self, data):
@@ -23,5 +23,5 @@ class LaserFront(smach.State):
 			return 'closerFront'
 		elif min(self.ranges[114:171]) < 0.5:
 			return 'closerLeft'
-		else
+		else:
 			return 'far'
