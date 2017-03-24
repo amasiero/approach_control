41d06335d60b25d168572b2be439674bdfc7cc8d
Andrey Masiero <amasiero@fei.edu.br>
2017-03-24 15:10:21 -0300

    Update on GenderDiscover

--- approach_control_people/nodes/approach_control_people/faces/GenderDiscover.py
+++ approach_control_people/nodes/approach_control_people/faces/GenderDiscover.py
@@ -3,6 +3,9 @@
 import cv2.cv
 import imutils
 import time
+import smach
+import smach_ros
+import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -25,4 +28,62 @@ class GenderDiscover(smach.State):
 		self.m.MakeRegularCluster(self.SIZE, self.SIZE, self.GRID, self.GRID)
 		self.m.MakeRegions()
 
+		self.ulbp_face = ULBP.ULBP(m)
+		self.ulbp_female = ULBP.ULBP(m)
+		self.ulbp_male = ULBP.ULBP(m)
+
+		self.ulbp_male.MakePattern(male_db())
+		self.ulbp_male.MakeHistogram()
+
+		self.ulbp_female.MakePattern(female_db())
+		self.ulbp_female.MakeHistogram()
+
+		self.gender = None
+
+
+	def is_woman(self, img):
+		self.ulbp_face.MakePattern(img)
+		self.ulbp_face.MakeHistogram()
+
+		return Authentify.Authentify(ulbp_face.histogram, ulbp_female.histogram, ulbp_male.histogram, lbp_utils.W) > 20.0
+
+	def callback(self, data):
+
+		try:
+			image = self.bridge.imgmsg_to_cv2(data, 'bgr8')
+		except CvBridgeError as e:
+			rospy.logerr(e)
+
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+		roi_gray = None
 		
+		faces = self.face_cascade.detectMultScale(
+			gray,
+			scaleFactor = 1.1,
+			minNeighbors = 10,
+			minSize = (100,100),
+			flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+		)
+
+		for (x, y, w, h) in faces:
+			x1 = x + int(w * .1)
+			x2 = x1 + int(w * .8)
+
+			y1 = y + int(h * .2)
+			y2 = y1 + int(h * .8)
+
+			roi_gray = cv2.resize(gray[y1:y2, x1:x2], (128, 128))
+
+			self.gender = 'man'
+			if is_woman(roi_gray):
+				self.gender = 'woman'
+
+
+	def execute(self, userdata):
+		rospy.Subscriber('/image_raw', Image, self.callback)
+		rospy.sleep(5)
+		if self.gender is not None:
+			return gender
+		else:
+			return 'fail'
--- approach_control_sm/nodes/sm_face_say_test.py
+++ approach_control_sm/nodes/sm_face_say_test.py
@@ -7,7 +7,7 @@ import smach_ros
 import time
 
 from approach_control_voice import Say
-from approach_control_people.faces import FaceFinder, PersonFaceCapture
+from approach_control_people.faces import FaceFinder, PersonFaceCapture, GenderDiscover
 
 def setup_sm():
 
@@ -28,7 +28,20 @@ def setup_sm():
 								transitions={'success' : 'THANKS_MSG', 'in_progress' : 'RECORD_FACE', 'fail' : 'done'})
 
 		smach.StateMachine.add('THANKS_MSG', Say.Say('Now I will remmenber you.'),
+								transitions={'spoke' : 'SEARCH_GENDER', 'mute' : 'done'})
+
+		smach.StateMachine.add('SEARCH_GENDER', GenderDiscover.GenderDiscover(),
+								transitions={'woman' : 'SAY_WOMAN', 'man' : 'SAY_MAN', 'fail' : 'SAY_NEITHER'})
+	
+		smach.StateMachine.add('SAY_WOMAN', Say.Say('I see that you are a woman.'),
 								transitions={'spoke' : 'done', 'mute' : 'done'})
+
+		smach.StateMachine.add('SAY_MAN', Say.Say('I see that you are a man.'),
+								transitions={'spoke' : 'done', 'mute' : 'done'})
+
+		smach.StateMachine.add('SAY_NEITHER', Say.Say('I can say if you are a woman or a man.'),
+								transitions={'spoke' : 'done', 'mute' : 'done'})
+
 
 	sis = smach_ros.IntrospectionServer('Judith_StateMachineServer', sm, '/SM_JUDITH')
 	sis.start()
