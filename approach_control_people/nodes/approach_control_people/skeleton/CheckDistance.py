6a9c58087795412375ea492ce9307bdd40f57dba
Andrey Masiero <andreymasiero@gmail.com>
2017-03-29 19:50:45 -0300

    CheckDistance ready for test. Needs a new SM

--- approach_control_people/nodes/approach_control_people/skeleton/CheckDistance.py
+++ approach_control_people/nodes/approach_control_people/skeleton/CheckDistance.py
@@ -8,8 +8,22 @@ from std_msgs.msg import Float64
 class CheckDistance(smach.State):
 
 	def __init__(self):
-		smach.State.__init__(self, outcomes=['far', 'closer', 'fail'])
+		smach.State.__init__(self, outcomes=['safety','far', 'closer', 'fail'])
 		self.distance = None
 
+	def callback(self, ndistance):
+		if ndistance.data < 1.1:
+			self.distance = 'closer'
+		elif ndistance.data > 1.8:
+			self.distance = 'far'
+		else:
+			self.distance = 'safety'
+
+
 	def execute(self, userdata):
-	 	
+	 	rospy.Subscriber('/torso_distance', Float64, self.callback)
+	 	rospy.sleep(5)
+	 	if self.distance is not None:
+	 		return self.distance
+	 	else:
+	 		return 'fail'
