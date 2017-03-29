4aec969cae6e3a26afa5fb3d6f70bf6217f021af
Andrey Masiero <andreymasiero@gmail.com>
2017-03-29 19:32:18 -0300

    SkeletonTracker renamed to CheckDistance

--- approach_control_people/nodes/approach_control_people/skeleton/CheckDistance.py
+++ approach_control_people/nodes/approach_control_people/skeleton/CheckDistance.py
@@ -5,11 +5,11 @@ import smach
 import smach_ros
 from std_msgs.msg import Float64
 
-class SkeletonTracker(smach.State):
+class CheckDistance(smach.State):
 
 	def __init__(self):
 		smach.State.__init__(self, outcomes=['far', 'closer', 'fail'])
 		self.distance = None
 
 	def execute(self, userdata):
-	 
+	 	
