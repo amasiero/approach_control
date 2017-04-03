e07dc92c9c9100e64a906117199e3dfb48907eb2
Andrey Masiero <amasiero@fei.edu.br>
2017-04-03 16:07:07 -0300

    SetInitialPosition State added to navigation package

---
+++ approach_control_navigation/nodes/approach_control_navigation/SetInitialPosition.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import rospy
+import rosparam
+import smach
+import os.path
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+class SetInitialPosition(smach.State):
+
+	def __init__(self, local = 'Initial Position', var = 0.05):
+		smach.State.__init__(self, outcomes = ['success', 'fail'])
+
+		params = rosparam.load_file(os.path.expanduser('~') + '/catkin_ws/src/approach_control/approach_control_config/config/locals.yaml')
+		self.initPose = rospy.Publisher('/initialPose', PoseWithCovarianceStamped, queue_size = 0)
+
+		if rospy.has_param(local):
+			local_values =  rospy.get_param(rospy.search_param(local))
+		else:
+			rospy.logerr('Param not found!')
+
+		# Define the initial position
+		self.initialPositionMsg = PoseWithCovarianceStamped()
+		self.initialPositionMsg.pose.position.x = local_values[0][0]
+		self.initialPositionMsg.pose.position.y = local_values[0][1]
+		self.initialPositionMsg.pose.orientation.w = local_values[1][3]
+		self.initialPositionMsg.pose.orientation.z = 0
+
+
+		for x in range(0, 35, 5):
+			self.initialPositionMsg.pose.covariance[x] = var
+
+	def execute(self, userdata):
+		try:
+			self.initPose.publish(self.initialPositionMsg)
+			return 'success'
+		except:
+			return 'fail'
