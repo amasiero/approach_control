3401f00b610a9603e4831bb1733ee6152a11613d
Andrey Masiero <amasiero@fei.edu.br>
2017-04-08 12:21:28 -0300

    small modification on record_gesture

--- approach_control_manipulator/nodes/record_gesture.py
+++ approach_control_manipulator/nodes/record_gesture.py
@@ -8,9 +8,35 @@ from dynamixel_msgs.msg import JointState
 from dynamixel_controllers.srv import TorqueEnable
 
 
-def main(args):
-    pass
+position_joint_1 = 0.0
+position_joint_2 = 0.0
+position_joint_3 = 0.0
+position_wrist   = 0.0
+position_base    = 0.0
 
+def callback_joint_1(data):
+    position_joint_1 = data.current_pos
+
+def callback_joint_2(data):
+    position_joint_2 = data.current_pos        
+
+def callback_joint_3(data):
+    position_joint_3 = data.current_pos        
+
+def callback_wrist(data):
+    position_wrist = data.current_pos        
+
+def callback_base(data):
+    position_base = data.current_pos        
+
+def main(args): 
+     
+     # Subscribers
+    rospy.Subscriber('/tilt2_controller/state', JointState, callback_joint_1, queue_size = 1)
+    rospy.Subscriber('/tilt3_controller/state', JointState, callback_joint_2, queue_size = 1)
+    rospy.Subscriber('/tilt4_controller/state', JointState, callback_joint_3, queue_size = 1)
+    rospy.Subscriber('/tilt5_controller/state', JointState, callback_wrist,   queue_size = 1)
+    rospy.Subscriber('/tilt_controller/state',  JointState, callback_base,    queue_size = 1)
 
 if __name__ == '__main__':
     rospy.init_node('record_geture', anonymous = True)
