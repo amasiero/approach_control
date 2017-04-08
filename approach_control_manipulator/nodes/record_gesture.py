3456a50a80bdd19f597f0bbb3f5ed52a859d91b0
Andrey Masiero <amasiero@fei.edu.br>
2017-04-08 13:56:37 -0300

    new launchs and small modifications on record_gesture

--- approach_control_manipulator/nodes/record_gesture.py
+++ approach_control_manipulator/nodes/record_gesture.py
@@ -8,35 +8,48 @@ from dynamixel_msgs.msg import JointState
 from dynamixel_controllers.srv import TorqueEnable
 
 
-position_joint_1 = 0.0
-position_joint_2 = 0.0
-position_joint_3 = 0.0
-position_wrist   = 0.0
-position_base    = 0.0
+position_joint_1
+position_joint_2
+position_joint_3
+position_wrist  
+position_base   
 
 def callback_joint_1(data):
+    global position_joint_1
     position_joint_1 = data.current_pos
 
 def callback_joint_2(data):
+    global position_joint_2
     position_joint_2 = data.current_pos        
 
 def callback_joint_3(data):
+    global position_joint_3       
     position_joint_3 = data.current_pos        
 
 def callback_wrist(data):
+    global position_wrist
     position_wrist = data.current_pos        
 
 def callback_base(data):
+    global position_base
     position_base = data.current_pos        
 
 def main(args): 
-     
+     global position_joint_1, position_joint_2, position_joint_3, position_wrist, position_base
+
      # Subscribers
     rospy.Subscriber('/tilt2_controller/state', JointState, callback_joint_1, queue_size = 1)
     rospy.Subscriber('/tilt3_controller/state', JointState, callback_joint_2, queue_size = 1)
     rospy.Subscriber('/tilt4_controller/state', JointState, callback_joint_3, queue_size = 1)
     rospy.Subscriber('/tilt5_controller/state', JointState, callback_wrist,   queue_size = 1)
     rospy.Subscriber('/tilt_controller/state',  JointState, callback_base,    queue_size = 1)
+
+    # Services
+    srv_joint_1 = rospy.ServiceProxy('/tilt2_controller/torque_enable', TorqueEnable, persistent=True)
+    srv_joint_2 = rospy.ServiceProxy('/tilt3_controller/torque_enable', TorqueEnable, persistent=True)
+    srv_joint_3 = rospy.ServiceProxy('/tilt4_controller/torque_enable', TorqueEnable, persistent=True)
+    srv_wrist = rospy.ServiceProxy('/tilt5_controller/torque_enable', TorqueEnable, persistent=True)
+    srv_base = rospy.ServiceProxy('/tilt_controller/torque_enable',  TorqueEnable, persistent=True)
 
 if __name__ == '__main__':
     rospy.init_node('record_geture', anonymous = True)
