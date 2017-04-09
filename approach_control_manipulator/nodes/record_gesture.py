7294469eb8b729027d4310645bba8e7888dd02c9
Andrey Masiero <andreymasiero@gmail.com>
2017-04-09 16:35:57 -0300

    Small improvement on record_gesture

--- approach_control_manipulator/nodes/record_gesture.py
+++ approach_control_manipulator/nodes/record_gesture.py
@@ -34,8 +34,9 @@ def callback_base(data):
     global position_base
     position_base = data.current_pos        
 
-def main(args): 
-     global position_joint_1, position_joint_2, position_joint_3, position_wrist, position_base
+def main(args):
+    # Giving global scope to servo position variables 
+    global position_joint_1, position_joint_2, position_joint_3, position_wrist, position_base
 
      # Subscribers
     rospy.Subscriber('/tilt2_controller/state', JointState, callback_joint_1, queue_size = 1)
@@ -48,8 +49,43 @@ def main(args):
     srv_joint_1 = rospy.ServiceProxy('/tilt2_controller/torque_enable', TorqueEnable, persistent=True)
     srv_joint_2 = rospy.ServiceProxy('/tilt3_controller/torque_enable', TorqueEnable, persistent=True)
     srv_joint_3 = rospy.ServiceProxy('/tilt4_controller/torque_enable', TorqueEnable, persistent=True)
-    srv_wrist = rospy.ServiceProxy('/tilt5_controller/torque_enable', TorqueEnable, persistent=True)
-    srv_base = rospy.ServiceProxy('/tilt_controller/torque_enable',  TorqueEnable, persistent=True)
+    srv_wrist   = rospy.ServiceProxy('/tilt5_controller/torque_enable', TorqueEnable, persistent=True)
+    srv_base    = rospy.ServiceProxy('/tilt_controller/torque_enable',  TorqueEnable, persistent=True)
+
+    # Reading yaml file for gesture
+    fname = os.path.expanduser('~') + '/catkin_ws/src/approach_control/approach_control_config/config/gestures.yaml'
+    stream = open(fname, 'r')
+    data = yaml.load(stream)
+    keys = data.keys()
+
+    # Calling a name for gesture
+    gesture_name = raw_input('Please inform a name for the gesture: ')
+    if not gesture_name:
+        rospy.logerr('It\'s required name for gesture ... ')
+    else:
+        # Calling user action
+        rospy.loginfo('To make a gesture you need to enable torque of the servos.')
+        enable_torque = raw_input('Would you like to enable torque? Type: yes | no | press enter to keep the actual torque state')
+        if enable_torque.lower() == 'yes':
+            srv_joint_1(True)
+            srv_joint_2(True)
+            srv_joint_3(True)
+            srv_wrist(True)
+            srv_base(True)
+        elif enable_torque.lower() == 'no':
+            srv_joint_1(False)
+            srv_joint_2(False)
+            srv_joint_3(False)
+            srv_wrist(False)
+            srv_base(False)
+
+        finish_gesture = ''
+        while finish_gesture.lower() != 'yes':
+            pass
+
+
+
+
 
 if __name__ == '__main__':
     rospy.init_node('record_geture', anonymous = True)
