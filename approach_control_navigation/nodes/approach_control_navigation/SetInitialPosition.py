fb79e9f954b6f6ca01805ef572833ec66f681415
Andrey Masiero <amasiero@fei.edu.br>
2017-05-06 12:39:03 -0300

    fix on test 01. Still not working SetInitialPosition, no more exception

--- approach_control_navigation/nodes/approach_control_navigation/SetInitialPosition.py
+++ approach_control_navigation/nodes/approach_control_navigation/SetInitialPosition.py
@@ -3,8 +3,9 @@
 import rospy
 import rosparam
 import smach
+import yaml
 import os.path
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovariance
 
 class SetInitialPosition(smach.State):
 
@@ -12,33 +13,36 @@ class SetInitialPosition(smach.State):
         smach.State.__init__(self, outcomes = ['success', 'fail'])
 
         # Reading yaml file for gesture
-        fname = os.path.expanduser('~') + '/catkin_ws/src/approach_control/approach_control_config/config/gestures.yaml'
+        fname = os.path.expanduser('~') + '/catkin_ws/src/approach_control/approach_control_config/config/locals.yaml'
         stream = open(fname, 'r')
-        data = yaml.load(stream)
-        keys = data.keys()
-        self.initPose = rospy.Publisher('/initialPose', PoseWithCovarianceStamped, queue_size = 0)
-
-        if local in keys:
-            local_values =  data[local]
-
-            # Define the initial position
-            self.initialPositionMsg = PoseWithCovarianceStamped()
-            self.initialPositionMsg.pose.position.x = local_values[0][0]
-            self.initialPositionMsg.pose.position.y = local_values[0][1]
-            self.initialPositionMsg.pose.orientation.w = local_values[1][3]
-            self.initialPositionMsg.pose.orientation.z = local_values[1][2]
-
-
-            for x in range(0, 35, 5):
-                self.initialPositionMsg.pose.covariance[x] = var
-        else:
-            rospy.logerr('Param not found!')
-
-        
+        self.data = yaml.load(stream)
+        self.local = local
+        self.keys = self.data.keys()
+        self.initialPositionMsg = PoseWithCovariance()
+        self.initPose = rospy.Publisher('/initialPose', PoseWithCovariance, queue_size = 0)
+        self.var = var
+                
 
     def execute(self, userdata):
+        
         try:
+                    
+            if self.local in self.keys:
+                local_values =  self.data[self.local] 
+                rospy.loginfo(local_values)
+                # Define the initial position
+                self.initialPositionMsg.pose.position.x = local_values[0][0]
+                self.initialPositionMsg.pose.position.y = local_values[0][1]
+                self.initialPositionMsg.pose.orientation.w = local_values[1][3]
+                self.initialPositionMsg.pose.orientation.z = local_values[1][2]
+
+
+                for x in range(0, 35, 5):
+                    self.initialPositionMsg.covariance[x] = self.var
+            else:
+                rospy.logerr('Local not found!')
             self.initPose.publish(self.initialPositionMsg)
             return 'success'
-        except:
+        except Exception as e:
+            rospy.logerr('Deu ruim! => ' + e)
             return 'fail'
